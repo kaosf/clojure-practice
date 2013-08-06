e64d686c6a6c76a2c9ff155e4c9c8ead7688f6ad
ka <ka.kaosf@gmail.com>
2013-08-07 01:27:10 +0900

    modify project name from "1.0.0-SNAPSHOT" to "myfirstapp"

--- myfirstapp/project.clj
+++ myfirstapp/project.clj
@@ -1,4 +1,4 @@
-(defproject myfirstapp "1.0.0-SNAPSHOT"
+(defproject myfirstapp "myfirstapp"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :main myfirstapp.core)
