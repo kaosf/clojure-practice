d0ace09f64feb77e4c192dbb2af5e732b4ad4d76
ka <ka.kaosf@gmail.com>
2013-08-08 21:33:18 +0900

    implement lazy-seq-fibo

--- book/4/functional.clj
+++ book/4/functional.clj
@@ -20,3 +20,11 @@
               current
               (recur next (+ current next) (dec n))))]
     (fib 0N 1N n)))
+
+(defn lazy-seq-fibo
+  ([]
+     (concat [0 1] (lazy-seq-fibo 0N 1N)))
+  ([a b]
+    (let [n (+ a b)]
+      (lazy-seq
+        (cons n (lazy-seq-fibo b n))))))
