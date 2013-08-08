41e19447e037c728263c3ba2c99045ae5cc79582
ka <ka.kaosf@gmail.com>
2013-08-08 11:38:32 +0900

    modify project.clj and core.clj
    enable to build a standalone runnable jar file

--- mysecondapp/src/mysecondapp/core.clj
+++ mysecondapp/src/mysecondapp/core.clj
@@ -1,6 +1,9 @@
-(ns mysecondapp.core)
+(ns mysecondapp.core (:gen-class))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main [& args]
+  (println "This is my second app. args: " args))
