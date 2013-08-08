d9e632b0cb25f7167bc9a8a026c39d07885f22fd
ka <ka.kaosf@gmail.com>
2013-08-08 11:28:06 +0900

    remove myfirstapp completely

--- myfirstapp/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject myfirstapp "myfirstapp"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :main myfirstapp.core)
--- myfirstapp/src/myfirstapp/core.clj
+++
@@ -1,5 +0,0 @@
-(ns myfirstapp.core
-(:gen-class))
-
-(defn -main [& args]
-(println "This is my first app. args: " args))
