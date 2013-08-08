e195cc70ac730b910668df6225d0208aa1a9727f
ka <ka.kaosf@gmail.com>
2013-08-08 20:38:42 +0900

    implement recur-fibo

--- book/4/functional.clj
+++ book/4/functional.clj
@@ -12,3 +12,11 @@
              current
              (fib next (+ current next) (dec n))))]
     (fib 0N 1N n)))
+
+(defn recur-fibo [n]
+  (letfn [(fib
+            [current next n]
+            (if (zero? n)
+              current
+              (recur next (+ current next) (dec n))))]
+    (fib 0N 1N n)))
