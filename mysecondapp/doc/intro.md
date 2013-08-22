8ce7d4bd96312ee961cb12d2ef8f14c4ad1dc411
ka <ka.kaosf@gmail.com>
2013-08-22 23:46:43 +0900

    remove mysecondapp completely

--- mysecondapp/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to mysecondapp
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- mysecondapp/src/mysecondapp/core.clj
+++
@@ -1,9 +0,0 @@
-(ns mysecondapp.core (:gen-class))
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-(defn -main [& args]
-  (println "This is my second app. args: " args))
